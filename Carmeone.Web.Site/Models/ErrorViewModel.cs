6076bb5457df656a2029f93145f686e79dac0c82
valerijvorobev <vorobjevvalera@gmail.com>
2023-01-08 19:45:45 +0300

    Удален проект aps.net mvc на asp.net web api (minimal app)

--- Carmeone.Web.Site/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Carmeone.Web.Site.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
